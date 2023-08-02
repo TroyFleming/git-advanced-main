783cb8d55e972380232380b08c508c48f7780597
Troy n' Essie Fleming <troy.fleming27@gmail.com>
2023-08-01 18:11:07 -0600

    checking changes

--- secrets_1.py
+++ secrets_1.py
@@ -1,2 +1,3 @@
 print("my secrets are here")
 print("are we sure this is working")
+print("it should not show these in commits")
--- test.py
+++ test.py
@@ -1,3 +1,5 @@
 print("hello troy, lets figure this out")
 print("why won't this commit")
 print("third commit test with ignore")
+print("add text")
+print("testing modify")
