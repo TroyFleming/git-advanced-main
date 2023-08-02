d2860d3d257b8dd6cb862001b751dfe4b79d4ee2
Troy n' Essie Fleming <troy.fleming27@gmail.com>
2023-08-01 18:05:12 -0600

    final ignore test

---
+++ secrets_1.py
@@ -0,0 +1,2 @@
+print("my secrets are here")
+print("are we sure this is working")
--- test.py
+++ test.py
@@ -1,2 +1,3 @@
 print("hello troy, lets figure this out")
 print("why won't this commit")
+print("third commit test with ignore")
